5d833bd53994bbc7b1ad42eeccd0fd1b813d2764
Maniang Loum <maniloum4@gmail.com>
2023-01-26 11:46:40 +0100

    affichage dans le console message pour différencer les pages : client et serveur

---
+++ loumi/server.js
@@ -0,0 +1,5 @@
+"use strict";
+
+console.log(
+  "Bienvenue vous êtes dans la page. Ce message apparaitra dans la console car on va utiliser node.js. Penser à installer node express & all the stuff you may find useful for the project"
+);
