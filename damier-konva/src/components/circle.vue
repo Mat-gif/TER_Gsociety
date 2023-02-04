a99ad2407caee2d0c09b70ff6deb46ee8746d51b
Cazeres Mathieu <mathieucazeres@gmail.com>
2023-02-04 12:03:35 +0100

    vue_Konva pion et damier deplacement ok

---
+++ damier-konva/src/components/circle.vue
@@ -0,0 +1,18 @@
+<template>
+        <v-circle 
+            :config="{
+                x: 410,
+                y: 170,
+                radius: 50,
+                fill: 'blue'
+            }"
+             :stage="stage"
+        />
+  </template>
+  
+  <script>
+  export default 
+  {
+    props: ['stage'],
+  };
+  </script>
