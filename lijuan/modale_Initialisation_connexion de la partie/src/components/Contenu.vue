ee1df04628a4af136a5ec4aa5f17d3156bb92021
jianglijuan0110 <35961632+jianglijuan0110@users.noreply.github.com>
2023-03-08 15:46:36 +0100

    modale regles

--- lijuan/modale_Initialisation_connexion de la partie/src/components/Contenu.vue
+++ lijuan/modale_Initialisation_connexion de la partie/src/components/Contenu.vue
@@ -5,10 +5,12 @@
     <!--lire la valuer de revele et utiliser la méthode toggleModale-->
     <creer v-bind:revele="reveleCreer" v-bind:toggleModale="toggleCreer"></creer>
     <rejoindre v-bind:revele="reveleRejoindre" v-bind:toggleModale="toggleRejoindre"></rejoindre>
+    <regles v-bind:revele="reveleRegles" v-bind:toggleModale="toggleRegles"></regles>
 
     <!--créer un boutton ouvrir la modale, click le boutton en utilisant la méthode toggleModale-->
     <div v-on:click="toggleCreer" class="btn btn-success">Créer une partie</div>
     <div v-on:click="toggleRejoindre" class="btn btn-success">Rejoindre une partie</div>
+    <div v-on:click="toggleRegles" class="btn btn-success">Regles</div>
     </div>
    
 </template>
@@ -16,6 +18,7 @@
 <script>
     import Creer from './CreerPartie.vue'
     import Rejoindre from './RejoindrePartie.vue'
+    import Regles from './Regles.vue'
 
     export default{
         name: 'Contenu',
@@ -23,12 +26,14 @@
             return{
                 // donner la valeur de revele
                 reveleCreer:false,
-                reveleRejoindre:false
+                reveleRejoindre:false,
+                reveleRegles:false
             }
         },
         components:{
             'creer': Creer,
-            'rejoindre': Rejoindre
+            'rejoindre': Rejoindre,
+            'regles' : Regles
 
         },
         methods:{
@@ -38,6 +43,9 @@
             },
             toggleRejoindre:function(){
                 this.reveleRejoindre = !this.reveleRejoindre
+            },
+            toggleRegles:function(){
+                this.reveleRegles = !this.reveleRegles
             }
         }
     }
