955298f5744570cba5434daa9329894c295a2242
LOUM <98965342+mloum1@users.noreply.github.com>
2023-03-30 00:27:58 +0200

    ajout de la classe gestion tour de jeu

--- loumi/chatbox/src/module_Tour.js
+++ loumi/chatbox/src/module_Tour.js
@@ -10,25 +10,53 @@ export   class Player {
     this.position = positionStart;
   }
 
-  // classe des Joueurs chainé
 
 
 }
 //* 2. Création de la classe joueur chainé
-export class PlayerLinked extends Player {
-  constructor(username, host, socket, roomId, positionStart, nextPlayer) {
-    super(username, host, socket, roomId, positionStart);
-    this.nextPlayer = nextPlayer;
+export class PlayerLinked {
+  constructor(player) {
+    this.playerPrev = null;
+    this.player = player;
+    this.Playernext = null;
   }
+ 
 }
 
 
 //* 3. Création de la classe tour
 export class Tour {
-  constructor(player) {
-    this.player = player;
+  constructor(players, currentPlayer) {
+    this.players = players;
+    this.currentPlayer = currentPlayer;
   }
 
+  joueurSuivant() {
+    const index = this.players.indexOf(this.currentPlayer);
+    return this.players[(index + 1) % this.players.length];
+  }
+
+  changerJoueurActif() {
+    this.currentPlayer = this.joueurSuivant();
+  }
+
+  estFini() {
+    // Vérifie si l'un des joueurs a atteint le bord opposé du plateau
+    // si la position du joueur est supérieur ou égale à 8, le joueur a gagné ou la partie est terminée
+    if (this.currentPlayer.positionEnd >= 8) {
+      return true;
+    }
+  
+    // Vérifie si l'un des joueurs n'a plus de murs
+    for (let player of this.players) {
+      if (joueur.murs.length === 0) {
+        return true;
+      }
+    }
+  
+    // Si aucun des cas ci-dessus n'est vrai, le jeu n'est pas terminé
+    return false;
+  }
 }
 
 
