4473276394c11d5a40f0540d3e83548c8640922a
Cazeres Mathieu <mathieucazeres@gmail.com>
2023-05-03 18:15:55 +0200

    modif port

--- le_quoridor/node-server/index.js
+++ le_quoridor/node-server/index.js
@@ -4,7 +4,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const http = require('http');
 const server = http.createServer(app);
 const io = require("socket.io")(server);
