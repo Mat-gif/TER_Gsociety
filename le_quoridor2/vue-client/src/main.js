c744446e4d93f368b8116df8ef22a2ab52535139
Vico08lv <99902825+Vico08lv@users.noreply.github.com>
2023-05-13 22:47:44 +0200

    branche host
    unique fichier quoridor

--- le_quoridor2/vue-client/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import "bootstrap/dist/css/bootstrap.css"
-import App from './App.vue'
-import VueKonva from 'vue-konva'
-
-const app = createApp(App);
-app.use(VueKonva);
-app.mount('#app');
